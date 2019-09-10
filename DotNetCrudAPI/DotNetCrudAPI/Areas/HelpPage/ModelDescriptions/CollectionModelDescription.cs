8fbf98d13576c7795c2299717f67603bec29804f
Lucas <i.lucas.guillemette@gmail.com>
2019-09-10 09:53:24 -0400

    premier commit

---
+++ DotNetCrudAPI/DotNetCrudAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DotNetCrudAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ DotNetCrudAPI/DotNetCrudAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DotNetCrudAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
